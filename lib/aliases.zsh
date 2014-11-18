fb0f96a32af826adcc3102f4fd86dc7e6101c0b3
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2014-11-18 16:32:15 +0100

    adding new changes from various pull requests

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -1,35 +1,103 @@
-# Push and pop directories on directory stack
-alias pu='pushd'
-alias po='popd'
+# List directory contents
+alias sl=ls
+# List all files colorized in long format
+alias ll="ls -lF ${colorflag}"
+# List all files colorized in long format, including dot files
+alias la="ls -laF ${colorflag}"
+# List only directories
+alias lsd="ls -lF ${colorflag} | grep --color=never '^d'"
+# Always use color output for `ls`
+alias ls="command ls ${colorflag}"
+alias l='ls -a'
+alias l1='ls -1'
 
-# Basic directory operations
-alias ...='cd ../..'
-alias -- -='cd -'
+export LS_COLORS='no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:'
 
-# Super user
-alias _='sudo'
-alias please='sudo'
-
-#alias g='grep -in'
-
-# Show history
-if [ "$HIST_STAMPS" = "mm/dd/yyyy" ]
-then
-    alias history='fc -fl 1'
-elif [ "$HIST_STAMPS" = "dd.mm.yyyy" ]
-then
-    alias history='fc -El 1'
-elif [ "$HIST_STAMPS" = "yyyy-mm-dd" ]
-then
-    alias history='fc -il 1'
-else
-    alias history='fc -l 1'
+# Detect which `ls` flavor is in use
+if ls --color > /dev/null 2>&1; then # GNU `ls`
+	colorflag="--color"
+else # OS X `ls`
+	colorflag="-G"
 fi
-# List direcory contents
-alias lsa='ls -lah'
-alias l='ls -lah'
-alias ll='ls -lh'
-alias la='ls -lAh'
 
-alias afind='ack-grep -il'
+alias _="sudo"
+# Enable aliases to be sudo’ed
+alias sudo='sudo '
+# Get week number
+alias week='date +%V'
 
+if [ $(uname) = "Linux" ]
+then
+  alias ls="ls --color=auto"
+fi
+which gshuf &> /dev/null
+if [ $? -eq 0 ]
+then
+  alias shuf=gshuf
+fi
+
+alias c='clear'
+alias k='clear'
+alias cls='clear'
+
+alias edit="$EDITOR"
+alias pager="$PAGER"
+
+alias q='exit'
+
+alias irc="$IRC_CLIENT"
+
+alias rb='ruby'
+
+# Pianobar can be found here: http://github.com/PromyLOPh/pianobar/
+
+alias piano='pianobar'
+
+# Easier navigation: .., ..., ...., ....., ~ and -
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ~="cd ~" # `cd` is probably faster to type though
+alias -- -="cd -"
+
+# Shell History
+alias h='history'
+
+# Tree
+if [ ! -x "$(which tree 2>/dev/null)" ]
+then
+  alias tree="find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'"
+fi
+
+# Directory
+alias	md='mkdir -p'
+alias	rd='rmdir'
+# Shortcuts
+alias d="cd ~/Documents/Dropbox"
+alias dl="cd ~/Downloads"
+alias dt="cd ~/Desktop"
+alias p="cd ~/projects"
+alias doc="cd ~/Documents"
+alias dev="cd ~/Developer"
+alias app="cd /Applications"
+alias g="git"
+alias gpu="git pull upstream"
+alias gpu2="git push upstream"
+alias gpg="git pull github"
+alias gpg2="git push github"
+alias h="history"
+alias j="jobs"
+alias v="vim"
+alias o="open"
+alias oo="open ."
+alias htdocs="cd /Applications/MAMP/htdocs/"
+alias mysql="mysql --host localhost"
+alias mongod="mongod --dbpath ~/Applications/mongodb/data/db/"
+alias protege="sh /Applications/Protege\ 5.0/run.sh"
+
+
+# Control Plex Media Server
+alias startPlex="sudo launchctl load /Library/LaunchDaemons/com.plex.plexconnect.bash.plist"
+alias stopPlex="sudo launchctl unload /Library/LaunchDaemons/com.plex.plexconnect.bash.plist"
+alias checkPlex="sudo launchctl list | grep plexconnect"
