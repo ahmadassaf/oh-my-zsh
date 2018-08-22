ac65725e7f3a0cc2445524b5429e9fc97d5c79f7
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2018-08-22 16:24:49 +0100

    adjusting the installation script

--- tools/install.sh
+++ tools/install.sh
@@ -1,31 +1,22 @@
 main() {
-  # Use colors, but only if connected to a terminal, and that terminal
-  # supports them.
-  if which tput >/dev/null 2>&1; then
-      ncolors=$(tput colors)
-  fi
-  if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
-    RED="$(tput setaf 1)"
-    GREEN="$(tput setaf 2)"
-    YELLOW="$(tput setaf 3)"
-    BLUE="$(tput setaf 4)"
-    BOLD="$(tput bold)"
-    NORMAL="$(tput sgr0)"
-  else
-    RED=""
-    GREEN=""
-    YELLOW=""
-    BLUE=""
-    BOLD=""
-    NORMAL=""
-  fi
 
-  # Only enable exit-on-error after the non-critical colorization stuff,
-  # which may fail on systems lacking tput or terminfo
-  set -e
+  printf "\n\n${YELLOW}
+   ▄██████▄     ▄█    █▄           ▄▄▄▄███▄▄▄▄   ▄██   ▄         ▄███████▄     ▄████████    ▄█    █▄    
+███    ███   ███    ███        ▄██▀▀▀███▀▀▀██▄ ███   ██▄      ██▀     ▄██   ███    ███   ███    ███   
+███    ███   ███    ███        ███   ███   ███ ███▄▄▄███            ▄███▀   ███    █▀    ███    ███   
+███    ███  ▄███▄▄▄▄███▄▄      ███   ███   ███ ▀▀▀▀▀▀███       ▀█▀▄███▀▄▄   ███         ▄███▄▄▄▄███▄▄ 
+███    ███ ▀▀███▀▀▀▀███▀       ███   ███   ███ ▄██   ███        ▄███▀   ▀ ▀███████████ ▀▀███▀▀▀▀███▀  
+███    ███   ███    ███        ███   ███   ███ ███   ███      ▄███▀                ███   ███    ███   
+███    ███   ███    ███        ███   ███   ███ ███   ███      ███▄     ▄█    ▄█    ███   ███    ███   
+ ▀██████▀    ███    █▀          ▀█   ███   █▀   ▀█████▀        ▀████████▀  ▄████████▀    ███    █▀    
+                                                                                                      
+  ${NC}\n\n
+"
+  SOURCE_LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+  . ${SOURCE_LOCATION}/colors.sh
 
   if ! command -v zsh >/dev/null 2>&1; then
-    printf "${YELLOW}Zsh is not installed!${NORMAL} Please install zsh first!\n"
+    printf "${YELLOW}Zsh is not installed!${NC} Please install zsh first!\n"
     exit
   fi
 
@@ -34,7 +25,7 @@ main() {
   fi
 
   if [ -d "$ZSH" ]; then
-    printf "${YELLOW}You already have Oh My Zsh installed.${NORMAL}\n"
+    printf "${YELLOW}You already have Oh My Zsh installed.${NC}\n"
     printf "You'll need to remove $ZSH if you want to re-install.\n"
     exit
   fi
@@ -46,7 +37,7 @@ main() {
   # precedence over umasks except for filesystems mounted with option "noacl".
   umask g-w,o-w
 
-  printf "${BLUE}Cloning Oh My Zsh...${NORMAL}\n"
+  printf "${BLUE}Cloning Oh My Zsh...${NC}\n"
   command -v git >/dev/null 2>&1 || {
     echo "Error: git is not installed"
     exit 1
@@ -59,56 +50,44 @@ main() {
       exit 1
     fi
   fi
-  env git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git "$ZSH" || {
+  env git clone --depth=1 https://github.com/ahmadassaf/oh-my-zsh.git "$ZSH" || {
     printf "Error: git clone of oh-my-zsh repo failed\n"
     exit 1
   }
 
 
-  printf "${BLUE}Looking for an existing zsh config...${NORMAL}\n"
+  printf "${BLUE}Looking for an existing zsh config...${NC}\n"
   if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]; then
-    printf "${YELLOW}Found ~/.zshrc.${NORMAL} ${GREEN}Backing up to ~/.zshrc.pre-oh-my-zsh${NORMAL}\n";
+    printf "${YELLOW}Found ~/.zshrc.${NC} ${GREEN}Backing up to ~/.zshrc.pre-oh-my-zsh${NC}\n";
     mv ~/.zshrc ~/.zshrc.pre-oh-my-zsh;
   fi
 
-  printf "${BLUE}Using the Oh My Zsh template file and adding it to ~/.zshrc${NORMAL}\n"
+  printf "${BLUE}Using the Oh My Zsh template file and adding it to ~/.zshrc${NC}\n"
   cp "$ZSH"/templates/zshrc.zsh-template ~/.zshrc
   sed "/^export ZSH=/ c\\
   export ZSH=\"$ZSH\"
   " ~/.zshrc > ~/.zshrc-omztemp
   mv -f ~/.zshrc-omztemp ~/.zshrc
 
-  # If this user's login shell is not already "zsh", attempt to switch.
-  TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
-  if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
-    # If this platform provides a "chsh" command (not Cygwin), do it, man!
-    if hash chsh >/dev/null 2>&1; then
-      printf "${BLUE}Time to change your default shell to zsh!${NORMAL}\n"
-      chsh -s $(grep /zsh$ /etc/shells | tail -1)
-    # Else, suggest the user do so manually.
-    else
-      printf "I can't change your shell automatically because this system does not have chsh.\n"
-      printf "${BLUE}Please manually change your default shell to zsh!${NORMAL}\n"
+  echo ""
+  read -p "$(echo "${RED}Would you like to change your shell to Zsh${NC}") [Y/N] " -n 1 CHANGE_SHELL </dev/tty
+  if [[ $CHANGE_SHELL =~ ^[yY]$ ]]; then
+    # If this user's login shell is not already "zsh", attempt to switch.
+    TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
+    if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
+      # If this platform provides a "chsh" command (not Cygwin), do it, man!
+      if hash chsh >/dev/null 2>&1; then
+        printf "${BLUE}Time to change your default shell to zsh!${NC}\n"
+        chsh -s $(grep /zsh$ /etc/shells | tail -1)
+      # Else, suggest the user do so manually.
+      else
+        printf "I can't change your shell automatically because this system does not have chsh.\n"
+        printf "${BLUE}Please manually change your default shell to zsh!${NC}\n"
+      fi
     fi
   fi
 
-  printf "${GREEN}"
-  echo '         __                                     __   '
-  echo '  ____  / /_     ____ ___  __  __   ____  _____/ /_  '
-  echo ' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '
-  echo '/ /_/ / / / /  / / / / / / /_/ /    / /_(__  ) / / / '
-  echo '\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/  '
-  echo '                        /____/                       ....is now installed!'
-  echo ''
-  echo ''
-  echo 'Please look over the ~/.zshrc file to select plugins, themes, and options.'
-  echo ''
-  echo 'p.s. Follow us at https://twitter.com/ohmyzsh.'
-  echo ''
-  echo 'p.p.s. Get stickers and t-shirts at https://shop.planetargon.com.'
-  echo ''
-  printf "${NORMAL}"
-  env zsh -l
+  printf "\n${BLUE}oh-my-zsh is now installed ... Please look over the ~/.zshrc file to select plugins, themes, and options${NC}\n\n"
 }
 
 main
